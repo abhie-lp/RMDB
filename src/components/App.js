8f8ed04d0a65a9dfd6f611174fa7cbd4d9c0355c
AT <abhie.lp@gmail.com>
2020-06-19 00:30:22 +0530

    Create a GlobalStyle

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from "./elements/Header";
 import Home from './Home';
+import {GlobalStyle} from "./styles/GlobalStyle";
+
 
 const App = () => (
     <>
-    <Header />
-    <Home />
+        <Header />
+        <Home />
+        <GlobalStyle />
     </>
 );
 
