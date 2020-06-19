91ab0bee9b48bc21d0cf0d3a1b5cab4df68849db
AT <abhie.lp@gmail.com>
2020-06-20 04:18:32 +0530

    Added router for MovieThumb and Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from "./elements/Header";
 import Home from './Home';
 import {GlobalStyle} from "./styles/GlobalStyle";
+import {Router} from "@reach/router";
+import Movie from "./Movie";
+import NotFound from "./NotFound";
 
 
 const App = () => (
     <>
         <Header />
-        <Home />
+        <Router>
+            <Home path="/"/>
+            <Movie path="/:movieId" />
+            <NotFound default />
+        </Router>
         <GlobalStyle />
     </>
 );
