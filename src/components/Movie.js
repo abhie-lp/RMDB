757e7f42d9497b7d697a2d545b71b995eaa367d4
AT <abhie.lp@gmail.com>
2020-06-23 11:32:14 +0530

    Scaffolding of movie components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Actor from "./elements/Actor";
+import Navigation from "./elements/Navigation";
+import MovieInfo from "./elements/MovieInfo";
+import MovieInfoBar from "./elements/MovieInfoBar";
+import Grid from "./elements/Grid";
+import Spinner from "./elements/Spinner";
 
-const Movie = ({movieId}) => <div>Movie: {movieId}</div>
+const Movie = ({movieId}) => (
+    <>
+        <Navigation />
+        <MovieInfo />
+        <MovieInfoBar />
+        <Grid>
+            <Actor />
+        </Grid>
+        <Spinner />
+    </>
+)
 
 export default Movie;
